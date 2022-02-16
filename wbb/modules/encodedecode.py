bcab3a01a525626b28848a1d9ba9d091d3003d56
四宮 かぐや//مجهول// <99824993+AnonymousHm@users.noreply.github.com>
2022-02-17 00:05:16 +0530

    Add files via upload

---
+++ wbb/modules/encodedecode.py
@@ -0,0 +1,58 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+
+
+from wbb import app # This is bot's client
+from wbb import app2 # userbot client, import it if module is related to userbot
+from pyrogram import filters # pyrogram filters
+...
+
+
+# For /help menu
+__MODULE__ = "encodedecode"
+__HELP__ = """✘ Commands Available -
+/encode <text/reply to message>
+    encode the text
+/decode <text/reply to message>
+    decode the text
+"""
+
+
+@app.on_message(filters.command("encode"))
+async def encod(e):
+    match = e.pattern_match.group(1)
+    if not match and e.is_reply:
+        gt = await e.message.reply_text()
+        if gt.text:
+            match = gt.text
+    if not (match or e.is_reply):
+        return await eor(e, "`Give me Something to Encode..`")
+    byt = match.encode("ascii")
+    et = base64.b64encode(byt)
+    atc = et.decode("ascii")
+    await eor(e, f"**=>> Encoded Text :** `{match}`\n\n**=>> OUTPUT :**\n`{atc}`")
+
+
+
+@app.on_message(filters.command("decode"))
+async def encod(e):
+    match = e.pattern_match.group(1)
+    if not match and e.is_reply:
+        gt = await e.message.reply_text()
+        if gt.text:
+            match = gt.text
+    if not (match or e.is_reply):
+        return await eor(e, "`Give me Something to Decode..`")
+    byt = match.encode("ascii")
+    try:
+        et = base64.b64decode(byt)
+        atc = et.decode("ascii")
+        await eor(e, f"**=>> Decoded Text :** `{match}`\n\n**=>> OUTPUT :**\n`{atc}`")
+    except Exception as p:
+        await eor(e, "**ERROR :** " + str(p))
+
